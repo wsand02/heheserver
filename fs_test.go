0ffc7274f055a8d25a3a9e9ef8356424969dcd5c
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2026-03-09 18:45:06 +0100

    sub directory test

--- fs_test.go
+++ fs_test.go
@@ -35,6 +35,30 @@ func TestOpenErr(t *testing.T) {
 	}
 }
 
+func TestSubDirectory(t *testing.T) {
+	dir := t.TempDir()
+	os.WriteFile(filepath.Join(dir, ".heheignore"), []byte("secret.txt\n"), 0644)
+	os.WriteFile(filepath.Join(dir, "secret.txt"), []byte("shh"), 0644)
+	os.WriteFile(filepath.Join(dir, "public.txt"), []byte("hi"), 0644)
+	subdir := filepath.Join(dir, "subdir")
+	os.Mkdir(subdir, 0644)
+	os.WriteFile(filepath.Join(subdir, ".heheignore"), []byte("no.txt"), 0755)
+	os.WriteFile(filepath.Join(dir, "no.txt"), []byte("yes"), 0644)
+	os.WriteFile(filepath.Join(subdir, "no.txt"), []byte("yes"), 0644)
+	hfs := Dir(dir)
+
+	_, err := hfs.Open("subdir/no.txt")
+	if err == nil {
+		t.Fatal("expected error")
+	}
+
+	file, err := hfs.Open("no.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+}
+
 func TestReaddirIgnoreFile(t *testing.T) {
 	dir := t.TempDir()
 	os.WriteFile(filepath.Join(dir, ".heheignore"), []byte("secret.txt\n"), 0644)
