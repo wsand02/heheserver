f08944e17a8822693c17ac2f8db572e4d0b3fb89
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2026-04-10 02:47:27 +0200

    remove thingallery

--- internal/server/handlers/gallery.go
+++ internal/server/handlers/gallery.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"path/filepath"
 	"strings"
 
 	"github.com/wsand02/heheserver/internal/fs"
@@ -12,10 +14,41 @@ type GalleryContext struct {
 	Items []GalleryItem
 }
 
+var imgExt = []string{".jpg", ".jpeg", ".png", ".webp", ".svg"}
+var vidExt = []string{".mov", ".mp4", ".m4v"}
+var audExt = []string{".mp3", ".wav", ".ogg", ".m4a"}
+
 type GalleryItem struct {
 	Filename string
 	IsDir    bool
 	Path     string
+}
+
+func (gi *GalleryItem) IsImage() bool {
+	for _, ie := range imgExt {
+		if filepath.Ext(strings.ToLower(gi.Filename)) == ie {
+			fmt.Println(filepath.Ext(strings.ToLower(gi.Filename)))
+			return true
+		}
+	}
+	return false
+}
+
+func (gi *GalleryItem) IsVideo() bool {
+	for _, ie := range vidExt {
+		if filepath.Ext(strings.ToLower(gi.Filename)) == ie {
+			return true
+		}
+	}
+	return false
+}
+func (gi *GalleryItem) IsAudio() bool {
+	for _, ie := range audExt {
+		if filepath.Ext(strings.ToLower(gi.Filename)) == ie {
+			return true
+		}
+	}
+	return false
 }
 
 func (gi *GalleryItem) GetUrl() string {
@@ -36,6 +69,7 @@ func GalleryHandler(w http.ResponseWriter, r *http.Request, ctx string, hfs *fs.
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	var gc GalleryContext
 	for i := 0; i < len(dirlis); i++ {
 		gc.Items = append(gc.Items, GalleryItem{Filename: dirlis[i].Name(), IsDir: dirlis[i].IsDir(), Path: ctx})
